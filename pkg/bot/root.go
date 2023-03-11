436caa71ad784cdd661a1c3a7ad9fa320beddfae
なるみ <4680567+narumiruna@users.noreply.github.com>
2023-03-11 08:02:15 +0000

    simplify

--- pkg/bot/root.go
+++ pkg/bot/root.go
@@ -41,45 +41,27 @@ func (g *ChatGPT) complete(ctx context.Context, messages Messages) (Messages, er
 	return messages, nil
 }
 
-func (g *ChatGPT) start(c tele.Context) error {
-	payload := c.Message().Payload
+func (g *ChatGPT) chat(c tele.Context) error {
+	messages := Messages{}
+	isReply := c.Message().IsReply()
+	log.Infof("isReply: %t", isReply)
 
-	messages := Messages{
-		{
-			Role:    openai.ChatMessageRoleUser,
-			Content: payload,
-		},
+	if isReply {
+		oldMessages, ok := g.messagesMap[c.Message().ReplyTo.ID]
+		if !ok {
+			return fmt.Errorf("no messages for ReplyTo.ID: %d", c.Message().ReplyTo.ID)
+		}
+		messages = append(messages, oldMessages...)
 	}
 
-	messages, err := g.complete(context.Background(), messages)
-	if err != nil {
-		return err
+	content := c.Message().Payload
+	if isReply {
+		content = c.Message().Text
 	}
-
-	teleMessage, err := c.Bot().Reply(c.Message(), messages.LastContent())
-	if err != nil {
-		return err
-	}
-	g.messagesMap[teleMessage.ID] = messages
-	return nil
-}
-
-func (g *ChatGPT) reply(c tele.Context) error {
-	if !c.Message().IsReply() {
-		log.Infof("Message is not a reply: %+v", c.Message())
-		return nil
-	}
-
-	replyToID := c.Message().ReplyTo.ID
-	log.Infof("Reply ID: %d", replyToID)
-	messages, ok := g.messagesMap[replyToID]
-	if !ok {
-		return fmt.Errorf("no messages for reply ID: %d", replyToID)
-	}
-
+	log.Infof("user content: %s", content)
 	messages = append(messages, openai.ChatCompletionMessage{
 		Role:    openai.ChatMessageRoleUser,
-		Content: c.Message().Text,
+		Content: content,
 	})
 
 	messages, err := g.complete(context.Background(), messages)
@@ -112,8 +94,8 @@ func Execute() {
 
 	chatGPT := NewChatGPT(openaiAPIKey)
 
-	bot.Handle("/gpt", chatGPT.start)
-	bot.Handle(tele.OnText, chatGPT.reply)
+	bot.Handle("/gpt", chatGPT.chat)
+	bot.Handle(tele.OnText, chatGPT.chat)
 
 	log.Infof("Starting bot")
 	bot.Start()
