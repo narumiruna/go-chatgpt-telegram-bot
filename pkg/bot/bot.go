f5343c3a96c992a3c1679326881bf5dc02312242
なるみ <4680567+narumiruna@users.noreply.github.com>
2023-05-02 13:34:38 +0000

    add en and jp command

--- pkg/bot/bot.go
+++ pkg/bot/bot.go
@@ -63,6 +63,8 @@ func Execute() {
 	bot.Handle("/set", chatGPTService.HandleSetCommand)
 	bot.Handle("/help", HandleHelpCommand)
 	bot.Handle("/tc", chatGPTService.HandleTCCommand)
+	bot.Handle("/en", chatGPTService.HandleENCommand)
+	bot.Handle("/jp", chatGPTService.HandleJPCommand)
 
 	if config.EnableImageCommand {
 		log.Infof("enabling /image command")
