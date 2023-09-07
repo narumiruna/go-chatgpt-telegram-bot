7b3b08b6f47d217e2660c481f7e26f0228db196f
なるみ <toucans-cutouts0f@icloud.com>
2023-09-08 00:31:52 +0800

    update help command

--- pkg/bot/help.go
+++ pkg/bot/help.go
@@ -5,7 +5,6 @@ import tele "gopkg.in/telebot.v3"
 func HandleHelpCommand(c tele.Context) error {
 	return c.Reply(`/help - show this help message
 /gpt <message> - start a new chat
-/set <message> - set the system content
 /tc <message> - translate message to Chinese
 /en <message> - translate message to English
 /jp <message> - translate message to Japanese
